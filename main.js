3a6d5aa0bca0886797ada00e780a9d827605a70d
Nicolaas Nel <nicon@infoslips.com>
2019-12-02 10:50:40 +0200

    Moved Scripts to main and renabled disabled scripts.

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
-// const HOST = location.origin.replace(/^http/, 'ws').replace(":5500", "");
-// // const socket = new WebSocket(HOST + ":8080");
-// const socket = new WebSocket("wss://192.168.15.164:8080");
+const HOST = location.origin.replace(/^http/, 'ws')
+const ws = new WebSocket(HOST);
 
-// socket.addEventListener('message', event => {
-//     let message = JSON.parse(event.data);
-//     $("#chat").append(`<tr id="msg${messagesCount}"><td>${message.userName}: ${message.content}</td></tr>`);
-// });
+ws.onmessage = function (event) {
+    $('#server-time').innerHTML = 'Server time: ' + event.data;
+};
 
-// socket.onmessage = function (event) {
-//     console.log('Server time: ' + event.data);
-// };
+ws.addEventListener('message', event => {
+    let message = JSON.parse(event.data);
+    $("#chat").append(`<tr id="msg${messagesCount}"><td>${message.userName}: ${message.content}</td></tr>`);
+});
 
-// socket.addEventListener('open', () => {
-//     // sendMessage(socket, "NicmeisteR", message);
-// });
+ws.addEventListener('open', () => {
+    // sendMessage(ws, "NicmeisteR", message);
+});
 
-// function sendMessage(socket, userName, content) {
-//     socket.send(
-//       JSON.stringify({
-//         userName,
-//         content
-//       })
-//     )
-// }
+function sendMessage(ws, userName, content) {
+    ws.send(
+      JSON.stringify({
+        userName,
+        content
+      })
+    )
+}
 
-// let array = [];
-// let messagesCount = 0;
+let array = [];
+let messagesCount = 0;
 
-// $('#send').click(() => {
-//     messagesCount += 1;
-//     sendMessage(socket, $('#username').val(), $('#message').val());
-// })
+$('#send').click(() => {
+    messagesCount += 1;
+    sendMessage(ws, $('#username').val(), $('#message').val());
+})
