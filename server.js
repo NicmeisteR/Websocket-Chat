def2abe98e275323d9ffe5700555ad6c60ce958a
Nicolaas Nel <nicon@infoslips.com>
2019-12-02 10:42:25 +0200

    Added new directory location.

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ const SocketServer = require('ws').Server;
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, '/docs');
+const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
+  .use(express.static(__dirname + '/'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
