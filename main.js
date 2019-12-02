c00c96fb96282eb44b936b702ede8176060ba4d2
Nicolaas Nel <nicon@infoslips.com>
2019-12-02 10:31:47 +0200

    Moved HTML to Root.

---
+++ main.js
@@ -0,0 +1,33 @@
+// const HOST = location.origin.replace(/^http/, 'ws').replace(":5500", "");
+// // const socket = new WebSocket(HOST + ":8080");
+// const socket = new WebSocket("wss://192.168.15.164:8080");
+
+// socket.addEventListener('message', event => {
+//     let message = JSON.parse(event.data);
+//     $("#chat").append(`<tr id="msg${messagesCount}"><td>${message.userName}: ${message.content}</td></tr>`);
+// });
+
+// socket.onmessage = function (event) {
+//     console.log('Server time: ' + event.data);
+// };
+
+// socket.addEventListener('open', () => {
+//     // sendMessage(socket, "NicmeisteR", message);
+// });
+
+// function sendMessage(socket, userName, content) {
+//     socket.send(
+//       JSON.stringify({
+//         userName,
+//         content
+//       })
+//     )
+// }
+
+// let array = [];
+// let messagesCount = 0;
+
+// $('#send').click(() => {
+//     messagesCount += 1;
+//     sendMessage(socket, $('#username').val(), $('#message').val());
+// })
--- server.js
+++ server.js
@@ -18,28 +18,28 @@
 //     socket.send(`${message.userName} is saying: ${message.content}`);
 // }
 
-// 'use strict';
+'use strict';
 
-// const express = require('express');
-// const SocketServer = require('ws').Server;
-// const path = require('path');
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
 
-// const PORT = process.env.PORT || 3000;
-// const INDEX = path.join(__dirname, '/docs/index.html');
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
-// const server = express()
-//   .use((req, res) => res.sendFile(INDEX) )
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-// const wss = new SocketServer({ server });
+const wss = new SocketServer({ server });
 
-// wss.on('connection', (ws) => {
-//   console.log('Client connected');
-//   ws.on('close', () => console.log('Client disconnected'));
-// });
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
 
-// setInterval(() => {
-//   wss.clients.forEach((client) => {
-//     client.send(new Date().toTimeString());
-//   });
-// }, 1000);
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
