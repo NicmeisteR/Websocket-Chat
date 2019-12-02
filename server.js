2d23dcde22f4532eb2300cea4bdb0ed6b7b13286
Nicolaas Nel <nicon@infoslips.com>
2019-12-02 10:37:16 +0200

    Moved HTML to docs.

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const SocketServer = require('ws').Server;
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const INDEX = path.join(__dirname, '/docs');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
